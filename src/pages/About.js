374cab96da61de0a48e1f9f6241af02843cc4474
MANEA <aissatoumane17@gmail.com>
2022-04-20 11:40:02 +0200

    ajout des styles

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1> À PROPOS </h1>
+            <br />
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem doloremque est recusandae deserunt sed atque sequi nobis ab enim dignissimos expedita suscipit architecto, dolor nostrum excepturi, quo numquam possimus eaque dicta quam sit. Optio neque labore suscipit voluptatum nisi unde vitae, cum quo quod placeat ut magni rem error molestias, blanditiis quas libero vel voluptate, illum itaque dignissimos dolore sed iste atque? Asperiores error laudantium commodi fugit qui beatae eum similique quas, dicta eos minima reprehenderit tempora labore recusandae nisi, quia explicabo sapiente. Inventore quibusdam nulla commodi distinctio sint incidunt, hic totam ipsam iure illum mollitia nesciunt, amet sit porro.
+            </p>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero veniam sit corrupti cupiditate tenetur corporis in, tempora dolore laborum! Doloribus hic aut natus in eveniet corrupti, aliquid id sit voluptates culpa nam reprehenderit amet consequatur eius autem enim fuga nisi cupiditate beatae rerum officiis voluptatem dolores numquam? At porro sunt amet minima modi ex dignissimos quia distinctio laboriosam molestiae aspernatur illo, odio ad nostrum iste sequi soluta eius omnis. Deserunt sint sed, debitis iure quasi vero laborum rem laudantium at, architecto enim voluptatem veniam cupiditate. Dolores, nisi. Magnam quisquam vel ipsam sapiente earum distinctio consequatur eaque officiis maxime, ex ipsa?
+            </p>
+        </div>
+    );
+};
+
+export default About;
